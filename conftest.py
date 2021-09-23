883d28d4a8768f174a0af082e25247e5c2622100
andreybannikov <andrewnnov@yandex.ru>
2021-09-23 21:53:09 +0300

    final project

--- conftest.py
+++ conftest.py
@@ -21,12 +21,13 @@ def browser(request):
         print("\nstart chrome browser for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
+        browser = webdriver.Chrome("C:\\Projects\\stepik_qa\\driver\\chromedriver.exe", options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
+        browser = webdriver.Firefox(executable_path="C:\\Projects\\stepik_qa\\driver\\gecko\\geckodriver.exe",
+                                    firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
